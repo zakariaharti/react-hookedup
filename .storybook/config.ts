cc61aa5d20b6cf1293154b51d9e98ee86811e185
zakariaharti <zakariya.developer@gmail.com>
2018-12-22 19:42:27 +0100

    init storybook

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+// @ts-ignore
+const req = require.context('../stories', true, /.stories.tsx$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename: any) => req(filename));
 }
 
 configure(loadStories, module);
---
+++ stories/index.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Welcome', module).add('to Storybook', () => <h1>hello world</h1>);
